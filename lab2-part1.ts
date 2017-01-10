6fbce6c62891dfd55dad5c37a312876580f2bc17
Mayowa Aina <maina12@uw.edu>
2017-01-10 10:53:47 -0800

    completed CheckingsAccount class code

---
+++ lab2-part1.ts
@@ -0,0 +1,25 @@
+class CheckingsAccount{
+    //fields
+    private myBalance:number; 
+    private myOwner:string; 
+
+    //constructor
+    constructor(theInitialBalance:number, theOwner:string){
+        this.myBalance = theInitialBalance; 
+        this.myOwner = theOwner; 
+    }
+
+    public deposit(theAmount:number) {
+        this.myBalance += theAmount; 
+    }
+
+    public withdraw(theAmount:number) {
+        this.myBalance = this.myBalance - theAmount;  
+    }
+
+    public checkBalance() {
+        console.log(`Owner: ${this.myOwner}`);
+        console.log(`Checking Account Balance: ${this.myBalance}`); 
+    }
+}
+
