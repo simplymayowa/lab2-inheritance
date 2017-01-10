6fd14e3a52bb4a3bb5a27ee28ec889929efdafc5
Mayowa Aina <maina12@uw.edu>
2017-01-10 10:59:22 -0800

    completed SavingsAccount class code

---
+++ lab2-part2.ts
@@ -0,0 +1,31 @@
+class SavingsAccount{
+    //fields
+    private myBalance:number; 
+    private myOwner:string;
+    private myWithdrawals:number;  
+
+    //constructor
+    constructor(theInitialBalance:number, theOwner:string){
+        this.myBalance = theInitialBalance; 
+        this.myOwner = theOwner;
+        this.myWithdrawals = 0;  
+    }
+
+    public deposit(theAmount:number) {
+        this.myBalance += theAmount; 
+    }
+
+    public withdraw(theAmount:number) {
+        this.myWithdrawals++; 
+        if(this.myWithdrawals > 3) {
+            console.log("Error: Account has reached withdrawal limit.")
+        }else{
+            this.myBalance = this.myBalance - theAmount;  
+        }
+    }
+
+    public checkBalance() {
+        console.log(`Owner: ${this.myOwner}`);
+        console.log(`Checking Account Balance: ${this.myBalance}`); 
+    }
+}
